249741a937805e9a5e7df7a1489dad3d3babe972
sheren <sheren.t@hotmail.com>
2017-11-10 09:08:42 +1100

    complete for now

---
+++ README.md
@@ -0,0 +1,32 @@
+#Title: Tic Tac Toe
+
+###Summary
+-This project is Project0 for the General Assembly Web Development Immersive course WDi24.
+-This project uses JavaScript, jQuery, HTML and CSS to build the logic and design for the game.
+-Three files were used for this project: index.html, style.css and main.js.
+-This is a very basic Tic Tac Toe game with no special features.
+
+
+###Prerequisites
+-This game is available online through the GitHub website available here:
+-Best to be used with the latest Chrome browser but is
+
+
+###Running Tests??
+-Could run a series of coordinates for each player and a series of different scenarios for each game.
+-Test for each function with test coordinates or log/call functions.
+
+
+###Features to add
+
+
+
+
+
+###License
+
+
+
+###Acknowledgement
+-Used the Timer code by Sean Baines from CodePen.
+-Used code for neon light effects from Online Tutorials on YouTube.
--- js/main.js
+++ js/main.js
@@ -8,17 +8,30 @@ console.log("Tic Tac Toe");
     ['7', '8', '9']
 
   ];
+  $(".X").hide();
+  $(".O").hide();
 
   let playerTurn = true;
+  let gameOver = false;
 
     $('.square').on('click', function() {
+      if (gameOver === false) {
+        if ( playerTurn === true && gameBoard[$(this).data('array')][$(this).data('index')] !== 'X' && gameBoard[$(this).data('array')][$(this).data('index')] !==  'O' ) {
+        player1($(this).data('array'), $(this).data('index'));
+        $(this).append($(`<div class="X">
+          <div class="cross slash1"></div>
+          <div class="cross slash2"></div>
+          </div>`));
+        } else if (playerTurn === false && gameBoard[$(this).data('array')][$(this).data('index')] !== 'X' && gameBoard[$(this).data('array')][$(this).data('index')] !==  'O'){
+        player2($(this).data('array'), $(this).data('index'));
+        $(this).append($(`<div class="O">
+            <svg id="round" class="noughts" width="120" height="120">
+            <circle cx="60" cy="60" r="40" stroke-width="15"/>
+            </div>`))
+        }
+      }
+      console.log('clicked');
 
-      if ( playerTurn === true ) {
-      player1($(this).data('array'), $(this).data('index'));
-      } else {
-      player2($(this).data('array'), $(this).data('index'));
-    }
-    console.log('clicked');
     });
 
     const player1 = function( array, index ) {
@@ -26,8 +39,10 @@ console.log("Tic Tac Toe");
 
           if ( gameBoard[array][index] !== 'X' && gameBoard[array][index] !== 'O' ) {
             gameBoard[array][index] = 'X';
+
             playerTurn = false;
             winner();
+
           } else {
             console.log("Square taken, please try another one!");
           }
@@ -42,6 +57,7 @@ console.log("Tic Tac Toe");
 
         if ( gameBoard[array][index] !== 'X' && gameBoard[array][index] !== 'O' ) {
           gameBoard[array][index] = 'O';
+
           playerTurn = true;
           winner();
         } else {
@@ -53,62 +69,99 @@ console.log("Tic Tac Toe");
       }
     };
 
+  const winnerNotification = function(player) {
+    let notification = $(`<div class='notification'><p>Winner!</br>${player}</p></div>`);
+    $("body").append(notification);
+  };
+
   const winner = function() {
 
-    if (gameBoard[0][0] === gameBoard[0][1] && gameBoard[0][2] === gameBoard[0][1]) {
-        if (gameBoard[0][0] === 'X') {
-      console.log(`player 1 wins!`);
-        } else {
-      console.log(`player 2 wins`);
-        }
+        if (gameBoard[0][0] === gameBoard[0][1] && gameBoard[0][2] === gameBoard[0][1]) {
+            if (gameBoard[0][0] === 'X') {
+          console.log(`player 1 wins!`);
+          gameOver = true;
+          winnerNotification('player 1');
+            } else {
+          console.log(`player 2 wins`);
+          gameOver = true;
+          winnerNotification('player 2');
+            }
 
-    } else if (gameBoard[0][0] === gameBoard[1][1] && gameBoard[2][2] === gameBoard[1][1]) {
-        if (gameBoard[0][0] === 'X') {
-      console.log(`player 1 wins!`);
-        } else {
-      console.log(`player 2 wins`);
-        }
+        } else if (gameBoard[0][0] === gameBoard[1][1] && gameBoard[2][2] === gameBoard[1][1]) {
+            if (gameBoard[0][0] === 'X') {
+          console.log(`player 1 wins!`);
+          gameOver = true;
+          winnerNotification('player 1');
+            } else {
+          console.log(`player 2 wins`);
+          gameOver = true;
+          winnerNotification('player 2');
+            }
 
-    } else if (gameBoard[0][0] === gameBoard[1][0] && gameBoard[2][0] === gameBoard[1][0]) {
-        if (gameBoard[0][0] === 'X') {
-      console.log(`player 1 wins!`);
-        } else {
-      console.log(`player 2 wins`);
-        }
+        } else if (gameBoard[0][0] === gameBoard[1][0] && gameBoard[2][0] === gameBoard[1][0]) {
+            if (gameBoard[0][0] === 'X') {
+          console.log(`player 1 wins!`);
+          gameOver = true;
+          winnerNotification('player 1');
+            } else {
+          console.log(`player 2 wins`);
+          gameOver = true;
+          winnerNotification('player 2');
+            }
 
-    } else if (gameBoard[0][1] === gameBoard[1][1] && gameBoard[2][1] === gameBoard[1][1]) {
-        if (gameBoard[0][1] === 'X') {
-      console.log(`player 1 wins!`);
-        } else {
-      console.log(`player 2 wins`);
-        }
+        } else if (gameBoard[0][1] === gameBoard[1][1] && gameBoard[2][1] === gameBoard[1][1]) {
+            if (gameBoard[0][1] === 'X') {
+          console.log(`player 1 wins!`);
+          gameOver = true;
+          winnerNotification('player 1');
+            } else {
+          console.log(`player 2 wins`);
+          gameOver = true;
+          winnerNotification('player 2');
+            }
 
-    } else if (gameBoard[0][2] === gameBoard[1][2] && gameBoard[2][2] === gameBoard[1][2]) {
-        if (gameBoard[0][2] === 'X') {
-      console.log(`player 1 wins!`);
-        } else {
-      console.log(`player 2 wins`);
-        }
+        } else if (gameBoard[0][2] === gameBoard[1][2] && gameBoard[2][2] === gameBoard[1][2]) {
+            if (gameBoard[0][2] === 'X') {
+          console.log(`player 1 wins!`);
+          gameOver = true;
+          winnerNotification('player 1');
+            } else {
+          console.log(`player 2 wins`);
+          gameOver = true;
+          winnerNotification('player 2');
+            }
 
-    } else if (gameBoard[0][2] === gameBoard[1][1] && gameBoard[2][0] === gameBoard[1][1]) {
-        if (gameBoard[0][2] === 'X') {
-      console.log(`player 1 wins!`);
-        } else {
-      console.log(`player 2 wins`);
-        }
+        } else if (gameBoard[0][2] === gameBoard[1][1] && gameBoard[2][0] === gameBoard[1][1]) {
+            if (gameBoard[0][2] === 'X') {
+          console.log(`player 1 wins!`);
+          gameOver = true;
+          winnerNotification('player 1');
+            } else {
+          console.log(`player 2 wins`);
+          gameOver = true;
+          winnerNotification('player 2');
+            }
 
-    } else if (gameBoard[1][0] === gameBoard[1][1] && gameBoard[1][2] === gameBoard[1][1]) {
-        if (gameBoard[1][0] === 'X') {
-      console.log(`player 1 wins!`);
-        } else {
-      console.log(`player 2 wins`);
-        }
+        } else if (gameBoard[1][0] === gameBoard[1][1] && gameBoard[1][2] === gameBoard[1][1]) {
+            if (gameBoard[1][0] === 'X') {
+          console.log(`player 1 wins!`);
+          gameOver = true;
+          winnerNotification('player 1');
+            } else {
+          console.log(`player 2 wins`);
+          gameOver = true;
+          winnerNotification('player 2');
+            }
 
-    } else if (gameBoard[2][0] === gameBoard[2][1] && gameBoard[2][2] === gameBoard[2][1]) {
-        if (gameBoard[2][0] === 'X') {
-      console.log(`player 1 wins!`);
-        } else {
-      console.log(`player 2 wins`);
+        } else if (gameBoard[2][0] === gameBoard[2][1] && gameBoard[2][2] === gameBoard[2][1]) {
+            if (gameBoard[2][0] === 'X') {
+          console.log(`player 1 wins!`);
+          gameOver = true;
+          winnerNotification('player 1');
+            } else {
+          console.log(`player 2 wins`);
+          gameOver = true;
+          winnerNotification('player 2');
+            }
         }
-    }
   };
